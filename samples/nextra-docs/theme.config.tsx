4c8f483c262064807b4ea49fb3952cf34e89ae7d
Giuseppe La Torre <giuseppelt@live.com>
2023-06-02 03:24:21 +0200

    ref(samples/nextra): new integration scenarios

--- samples/nextra-docs/theme.config.tsx
+++ samples/nextra-docs/theme.config.tsx
@@ -1,5 +1,6 @@
 import { DocsThemeConfig } from "nextra-theme-docs";
-import { FeelbackTaggedMessage, PRESET_LIKE_DISLIKE } from "@feelback/react";
+import { FeelbackTaggedMessage, PRESET_FEELING, PRESET_FEEDBACK } from "@feelback/react";
+import { FeelbackYesNo, PRESET_LIKE_DISLIKE } from "@feelback/react";
 import "@feelback/react/styles/feelback.css";
 
 
@@ -9,13 +10,40 @@ export default {
         link: "https://github.com/shuding/nextra"
     },
     feedback: { content: null },
+
+    // scenario: evaluation form
     main: ({ children }) => {
         return (
             <>
                 {children}
                 <hr />
-                <FeelbackTaggedMessage title="Did you find this page useful?" contentSetId="" layout="inline" preset={PRESET_LIKE_DISLIKE} />
+                <FeelbackTaggedMessage contentSetId="your-content-set-id"
+                    layout="inline"
+                    preset={PRESET_FEELING}
+                    title="Did you find this page useful?"
+                />
             </>
         );
+    },
+
+    // scenario: page up votes
+    toc: {
+        extraContent: () =>
+            <>
+                <hr className="divider" />
+                <FeelbackYesNo contentSetId="your-content-set-id"
+                    preset={PRESET_LIKE_DISLIKE}
+                    textQuestion="Is this page useful?"
+                />
+            </>
+    },
+
+    // scenario: send feedback button
+    _toc: {
+        extraContent: () =>
+            <FeelbackTaggedMessage contentSetId="your-content-set-id"
+                layout="button-dialog"
+                preset={PRESET_FEEDBACK}
+            />
     }
 } as DocsThemeConfig
