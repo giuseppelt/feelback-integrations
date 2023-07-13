4575957ccaad585eeebd0b6912ca6777f7eef2ed
Giuseppe La Torre <giuseppelt@live.com>
2023-07-13 02:37:51 +0200

    feat(samples): new astro-strarlight sample with custom integration

---
+++ samples/astro-starlight/astro.config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import overrideComponentIntegration from "./src/overrideIntegration.mjs";
+
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    overrideComponentIntegration(),
+    starlight({
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: { service: { entrypoint: "astro/assets/services/sharp" } },
+});
